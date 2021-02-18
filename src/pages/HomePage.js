0bd6ad19aad7be3b15d2b6d4c995a0b62f6b61b6
luigitemu <luartemu@gmail.com>
2021-02-17 19:03:25 -0600

    add charts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { BandAdd } from '../components/BandAdd';
+import { BandChart } from '../components/BandChart';
 import { BandList } from "../components/BandList";
 import { SocketContext } from '../context/SocketContext';
 
@@ -34,8 +35,14 @@ function HomePage() {
 
         <h1>Band Names</h1>
         <hr />
+        <div className="row">
+          <div className="col">
+            <BandChart />
+          </div>
+        </div>
 
         <div className="row">
+
           <div className="col-8">
             <BandList />
           </div>
